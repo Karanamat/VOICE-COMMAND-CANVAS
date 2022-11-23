080ba2fe551ddbf1c94982345dbc9f8c628f4cb6
Karanamat <100451890+Karanamat@users.noreply.github.com>
2022-11-23 18:03:50 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+x = 0;
+y = 0;
+draw_apple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+ 
+    document.getElementById("status").innerHTML = "The Speech has been recognized as: " + content; 
+    if(content =="draw_apple" || content =="Apple")
+    for(var i = 1; i <= to_number; i++);
+    {
+      x = Math.floor(Math.random() * 700);
+      y = Math.floor(Math.random() * 400);
+      document.getElementById("status").innerHTML = "Started drawing circle "; 
+      image(apple, x, y, 50, 50);
+    }
+}
+
+function setup() {
+   canvas = createCanvas(900, 600);
+}
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
